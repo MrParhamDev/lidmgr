e07084e3a908ddde31ce2ab6afbe5ded0d1e626d
Empty <parhambarnamehnevis@gmail.com>
2023-10-02 15:30:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,28 @@
 
 import tkinter as tk
 
-window = tk.Tk()
 
-# here we go
+def show(*args):
+    for arg in args:
+        arg.pack()
+
+window = tk.Tk()
+title = tk.Label(
+    text="Linux Download Manager",
+    background="#d8e3eb",
+    fg="black",
+    width=150,
+    height=1,
+)
+
+download_label = tk.Label(text="Enter URL For Download")
+download_input = tk.Entry()
+download_spacer = tk.Label(text=" ")
+
+title.pack()
+download_label.pack(side=tk.LEFT)
+download_spacer.pack()
+download_spacer.pack()
+download_input.pack(side=tk.LEFT)
 
 window.mainloop()
